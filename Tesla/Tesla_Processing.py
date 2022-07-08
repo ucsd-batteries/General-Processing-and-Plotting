a7233301cb6390a1c88ff90f98e13bbe80e42566
mrchewyspree <lmathers@ucsd.edu>
2022-07-08 13:32:31 -0700

    Updated Tesla Processing
    Corresponds to Tesla test summary csv

--- Tesla/Tesla_Processing.py
+++ Tesla/Tesla_Processing.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import pandas as pd
 import datetime as dt
@@ -21,8 +22,8 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
     # get all cell voltages
 
     # first pack in series
-    n = 0
-    cell_voltages = data.iloc[:, 15 + n :15 + n + cell_num].to_numpy()
+    n=0
+    cell_voltages = data.iloc[:, 15 + n:15 + n + cell_num].to_numpy()
 
     # second pack in series
     # n = 6
@@ -46,7 +47,17 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
         elif Ipack[i]==0 and Ipack[i+1]!=0:
             Step = np.append(Step,i+1)
 
+       # ---- Plotting in case results are defective -------
+    fig, ax = plt.subplots()
+    ax.plot(Ipack, label='current')
+    ax.plot(cell_voltages[:, 0], label='voltage')
+    dots = [Ipack[int(s)] for s in Step[:]]
+    ax.scatter(Step[:], dots, color='red')
+    # ax.scatter(Step[start_idx], StartVs[0], color='black')
+    # ax.scatter(Step[end_idx], EndVs[0], color='black')
 
+    ax.legend()
+    plt.show()
     # --------to remove range of values---------------
     # print(cell_voltages.shape)
     #
@@ -54,15 +65,16 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
     # length = len(cell_voltages)-1
     # cell_v2 = cell_voltages[int(Step[10]):length, :]
     # cell_voltages = np.concatenate((cell_v1, cell_v2), axis=0)
-    #
-    # print(cell_voltages.shape)
-    #
+    # numpy.savetxt('new_aging_27.csv', cell_voltages)
+    # #
+    # # print(cell_voltages.shape)
+    # #
     # I1 = Ipack[0:int(Step[8])]
     # length = len(Ipack)-1
     # I2 = Ipack[int(Step[10]):length]
     # Ipack = np.concatenate((I1,I2),axis=0)
-    #
-    # # must recreate steps
+    # #
+    # # # must recreate steps
     # Step = []
     # for i in range(len(Ipack) - 1):
     #     if Ipack[i] != 0 and Ipack[i + 1] == 0:
@@ -74,20 +86,28 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
     # get StartVs, EndVs
     start_idx = 16
     end_idx = start_idx + 6
-    StartVs = cell_voltages[int(Step[16]-1),:]  # get the voltage at the height of the CCCV curve after constant voltage
-    EndVs = cell_voltages[int(Step[22]-1),:];  # get the voltage at the end of the CCCV curve after constant current discharge
+    StartVs = cell_voltages[int(Step[start_idx]-1),:]  # get the voltage at the height of the CCCV curve after constant voltage
+    EndVs = cell_voltages[int(Step[end_idx]-1),:]  # get the voltage at the end of the CCCV curve after constant current discharge
     # interpolate OCV-SOC curve to get SOCstart and SOCend values corresponding to StartVs and EndVs
     soc_curve = pd.read_csv(soc_curve_file)
     ocv = soc_curve.iloc[:,0].values.astype(float)
     soc = soc_curve.iloc[:,1].values.astype(float)
     ocv2=ocv[::-1]
     soc2=soc[::-1]
-
+    v_i1 = cell_voltages[int(Step[2]),:]
+    v_i2 = cell_voltages[int(Step[3]),:]
     endSOC= np.interp(EndVs, ocv2, soc2)
     startSOC= np.interp(StartVs, ocv2, soc2)
 
-
-
+    #cell voltage imbalance
+    CCCV_voltage = np.array(cell_voltages[int(Step[16]-1):int(Step[22]-1), :])
+    std_indiv = np.std(CCCV_voltage, axis=1)
+    std_final = np.mean(std_indiv, axis=0)
+    print('std_ccv +', std_final)
+    print('startv +', np.mean(StartVs))
+    print('endv +', np.mean(EndVs))
+    print('i1 +', np.mean(v_i1))
+    print('i2 +', np.mean(v_i2))
     # integrate discharge current to get discharge amp hours
     datetime_object = [dt.datetime.strptime(d[0:20]+d[24:28], '%a %b %d %H:%M:%S %Y') for d in data['Time']]
     tmstmp = [t.timestamp()/3600 for t in datetime_object]
@@ -97,11 +117,11 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
             ahDch[i+1] = ahDch[i] + .5*(tmstmp[i+2]-tmstmp[i+1])*(Ipack[i+2]+Ipack[i+1])
         else:
             ahDch[i+1] = ahDch[i]
-    DCH1 = ahDch[int(Step[16])]
+    DCH1 = ahDch[int(Step[start_idx])]
     DCHTotal = ahDch[-1]
     DCH2 = DCHTotal - DCH1
     # based on CC (const curr.) as specified by the Digatron test
-    CapDchAh = cc*(tmstmp[int(Step[17])]-tmstmp[int(Step[16])])
+    CapDchAh = cc*(tmstmp[int(Step[start_idx+1])]-tmstmp[int(Step[start_idx])])
 
     # print test start and end dates for logging purposes
     print('start: ' + data['Time'].iloc[0])
@@ -115,12 +135,7 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
     # determine test name
     test_name = data_file_path.split('_')
     test_name = test_name[-1].split('.')
-    print('st')
-    print(startSOC)
-    print('end')
-    print(endSOC)
-    print('dch')
-    print(CapDchAh)
+
     if isCalendarAging: 
         # get NP number
         x = re.findall("T1-\d+", data_file_path)
@@ -156,17 +171,7 @@ def getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isC
         summary.to_csv(summary_file, index=False, encoding='utf-8-sig')
 
 
-        # ---- Plotting in case results are defective -------
-    fig, ax = plt.subplots()
-    ax.plot(Ipack, label='current')
-    ax.plot(cell_voltages[:, 0], label='voltage')
-    dots = [Ipack[int(s)] for s in Step[17:20]]
-    ax.scatter(Step[17:20], dots, color='red')
-    # ax.scatter(Step[start_idx], StartVs[0], color='black')
-    # ax.scatter(Step[end_idx], EndVs[0], color='black')
 
-    ax.legend()
-    plt.show()
 # ------------------------------------ General Tesla Info ------------------------------
 # path to test summary file
 summary_file = r'/Users/liamk/OneDrive/Desktop/ESS LAB/Data/Tesla/Tesla_test_summary.csv'
@@ -180,17 +185,26 @@ cell_num=18
 # Value of constant current during characteriations
 cc = 40
 
+# set n to signify which battery is being processed in series
+# if cycle aging or first in calendar
+n = 0
+# second in calendar
+#n = 6
+# third in calendar
+#n = 12
+
+
 # ------------------------------------ Tesla Aging ------------------------------
 # path to where raw test csv is stored
 path = r'/Users/liamk/OneDrive/Desktop/Data to Process/'
-
+# path = r'F:/Tesla Data/'
 # name of csv file
-data_file = r'T1-7-2-6_Aging21.csv'
+data_file = r'T1-13_Safety-Cycle.csv'
 data_file_path = path + data_file
 # data_file2 = r'Tesla_Aging16_2.csv'
 # data_file_path_2 = path + data_file2
 
-getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc)
+# getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, n)
 
 
 # ------------------------------------ Tesla Calendar Aging ------------------------------
@@ -203,23 +217,25 @@ path = r'/Users/liamk/OneDrive/Desktop/Data to Process/'
 
 # # T1-3
 # data_file = r'December_T1-3_50.csv'
+# data_file = r'T1-3_50.csv'
 # data_file_path = path + 'T1-3/' + data_file
 # data_file2 = r'December_T1-3_50_2.csv'
 # data_file_path_2 = path + 'T1-3/' + data_file2
 
 # # T1-9
-# data_file = r'December_T1-9_75.csv'
+# data_file = r'T1-9_75SOC.csv'
 # data_file_path = path + 'T1-9/' + data_file
 
 # T1-10
-data_file = r'December_T1-10_90.csv'
-data_file_path = path + 'T1-10/' + data_file
+# data_file = r'T1-10_90SOC.csv'
+# data_file_path = path + 'T1-10/' + data_file
 
 # T1-12
-# data_file = r'Decemeber_T1-12_100.csv'
+# data_file = r'T1-12_100SOC.csv'
 # data_file_path = path + 'T1-12/' + data_file
+
 #
-# getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isCalendarAging=True)
+getAhAndCaps(data_file_path, cell_num, soc_curve_file, summary_file, cc, isCalendarAging=True)
 
 
 # ------------------------- For Processing Multiple Files ---------------------
